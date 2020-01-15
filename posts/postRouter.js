fdcf0ef483db5fb4c8b259815724c5af62e6cfce
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-01-15 15:15:22 -0500

    filling out post router .js

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,20 +2,27 @@ const express = require('express');
 
 const router = express.Router();
 
+/////////////////////////////////
+const PostInfo = require('./posts/postDb');
+
 router.get('/', (req, res) => {
   // do your magic!
+  PostInfo.get()
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  PostInfo.getById()
 });
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+  PostInfo.remove()
 });
 
 router.put('/:id', (req, res) => {
   // do your magic!
+  PostInfo.update()
 });
 
 // custom middleware
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,32 +2,44 @@ const express = require('express');
 
 const router = express.Router();
 
+////////////////////////////////////////////////
+
+const UserInfo = require('./users/userDb');
+
+
 router.post('/', (req, res) => {
   // do your magic!
+  UserInfo.g()
 });
 
 router.post('/:id/posts', (req, res) => {
   // do your magic!
+  UserInfo.g()
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  UserInfo.get()
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  UserInfo.getById()
 });
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+  UserInfo.getById()
 });
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+  UserInfo.remove()
 });
 
 router.put('/:id', (req, res) => {
   // do your magic!
+  UserInfo.update()
 });
 
 //custom middleware
